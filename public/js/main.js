0df4d4cbf56ed105832cdd1440f21cea936a27fe
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-07-11 03:56:51 +0430

    6

--- public/js/main.js
+++ public/js/main.js
@@ -3,39 +3,55 @@ $.get("./js/treedata.json", data => {
 });
 // createJstree(data);
 function createJstree(data) {
-	const r = f => `images/${f}.png`;
+	const o = f => ({ "icon": f ? `images/${f}.png` : "jstree-file", "valid_children": [] });
 	$('#tree-container').jstree({
 		core: {
 			data: data
 		},
 		// jstree-file
 		types: {
-			"":     { "icon": "jstree-file",  "valid_children": [] },
-			"txt":  { "icon": r("txt-16"),  "valid_children": [] },
-			"js":   { "icon": r("js-16"),  "valid_children": [] },
-			"ts":   { "icon": r("png-16"),  "valid_children": [] },
-			"html": { "icon": r("html-16"),  "valid_children": [] },
-			"htm":  { "icon": r("html-16"),  "valid_children": [] },
-			"md":   { "icon": r("png-16"),  "valid_children": [] },
-			"css":  { "icon": r("css-16"),  "valid_children": [] },
-			"scss": { "icon": r("png-16"),  "valid_children": [] },
-			"php":  { "icon": r("png-16"),  "valid_children": [] },
-			"c":    { "icon": r("png-16"),  "valid_children": [] },
-			"rb":   { "icon": r("png-16"),  "valid_children": [] },
-			"sql":  { "icon": r("png-16"),  "valid_children": [] },
-			"bat":  { "icon": r("png-16"),  "valid_children": [] },
-			"ps1":  { "icon": r("png-16"),  "valid_children": [] },
-			"json": { "icon": r("png-16"),  "valid_children": [] },
-			"xml":  { "icon": r("png-16"),  "valid_children": [] },
-			"conf": { "icon": r("png-16"),  "valid_children": [] },
-			"yml":  { "icon": r("png-16"),  "valid_children": [] },
-			"ai":   { "icon": r("png-16"),  "valid_children": [] },
-			"png":  { "icon": r("png-16"),  "valid_children": [] },
-			"jpg":  { "icon": r("jpg-16"),  "valid_children": [] },
-			"ico":  { "icon": r("png-16"),  "valid_children": [] },
-			"xlsm": { "icon": r("png-16"),  "valid_children": [] },
-			"zip":  { "icon": r("png-16"),  "valid_children": [] },
-			"handlebars": { "icon": r("png-16"),  "valid_children": [] }
+			"": o(),
+			"txt":  o("txt"),
+			"js":   o("js"),
+			"jsx":  o("js"),
+			"ts":   o("ts"),
+			"vue":  o("vue"),
+			"html": o("html"),
+			"htm":  o("html"),
+			"md":   o("md"),
+			"css":  o("css"),
+			"scss": o("scss"),
+			"php":  o("php"),
+			"c":    o("c"),
+			"rb":   o("rb"),
+			"sql":  o("sql"),
+			"bat":  o("bat"),
+			"ps1":  o("ps1"),
+			"json": o("json"),
+			"xml":  o("xml"),
+			"conf": o("conf"), //
+			"yaml": o("yml"),
+			"yml":  o("yml"),
+			"ai":   o("ai"),
+			"png":  o("png"),
+			"jpg":  o("jpg"),
+			"gif":  o("gif"),
+			"ico":  o("ico"),
+			"lnk":  o("lnk"),
+			"pdf":  o("pdf"),
+			"xls":  o("xlsx"),
+			"xlsx": o("xlsx"),
+			"xlsm": o("xlsx"),
+			"csv":  o("csv"),
+			"doc":  o("docx"),
+			"docx": o("docx"),
+			"pptx": o("pptx"),
+			"ppt":  o("pptx"),
+			"zip":  o("zip"),
+			"rar":  o("zip"),
+			"7zip": o("zip"),
+			"handlebars": o("handlebars"),
+			"hbs": o("handlebars")
 		},
 		plugins: ["types"]
 	});
@@ -43,57 +59,18 @@ function createJstree(data) {
 /*
 .browserslistrc
 .bowerrc
-.hbs
 .jshintrc
-.JPG
-.jsx
 .env
-.sql
 .mwb
 .bak
 .jade
-.handlebars
-.zip
 .rtlsrc
-.xml
 .babelrc
-.ico
-.yml
-.pdf
 .sublime-color-scheme
-.doc
-.pptx
-.xls
-.gif
-.vue
-.yaml
-.ps1
 .DS_Store
 .db
-
-
-
-
-
-.js
-.html
-.txt
-.bat
-.htm
-.png
-.c
-.ts
 .conf
-.ai
 .vpp
-.css
-.scss
-.lnk
-.xlsm
-.md
-.json
 .rb
-.php
-.jpg
 FileWithNoExtension
 */
